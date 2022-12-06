88db87a077bb6eac88e5c2deee0af1202cd80068
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-06 14:27:28 +0100

    Styles buttons again

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,10 @@ import styles from './styles/button.module.css'
 type ButtonProps = {
   name: string,
   href: string,
-  float?: 'left' | 'right' | 'none' | 'inline-start' | 'inline-end',
 };
 
 const Button = (props: ButtonProps) => {
-  return <div className={styles.buttonOuter} style={{ float: props.float ?? 'none' }}>
+  return <div className={styles.buttonOuter}>
     <div>
       <a href={ props.href }>
         { props.name }
