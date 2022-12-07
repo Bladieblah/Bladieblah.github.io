9f0e819a77b2ee1575addb0ae3dcd57b6b8086a7
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-07 18:46:51 +0100

    Add some icons and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './styles/layout.module.css'
 
 export default function Footer() {
   return (
-    <div className={styles.footer}/>
+    <div className={styles.footer}>
+      <a href='https://github.com/Bladieblah'><FontAwesomeIcon icon={faGithub}/> Bladieblah</a>
+      <a href='mailto:recruitment.mwesseling@gmail.com'><FontAwesomeIcon icon={faEnvelope}/> recruitment.mwesseling@gmail.com</a>
+      <a href='https://www.linkedin.com/in/matthijs-wesseling-779879199/'><FontAwesomeIcon icon={faLinkedin}/> Matthijs Wesseling</a>
+    </div>
   )
 }
