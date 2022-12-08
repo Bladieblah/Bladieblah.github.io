d30e72f7819eda50ae8cbb55fdd1e3e29858d75a
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-08 19:22:02 +0100

    Store video data in separate file

--- src/pages/music/index.tsx
+++ src/pages/music/index.tsx
@@ -1,12 +1,15 @@
-import Video from "../../components/Video";
+import VideoItem from "../../components/VideoItem";
+
+import video_data from "../videos/VideoData";
 
 export default function Music() {
   return (
     <>
       <h1>Music</h1>
-      <Video id='OQUkfxfNL9U'>
+      {video_data.map((d) => (<VideoItem id={d.id}>{d.description}</VideoItem>))}
+      {/* <VideoItem id='OQUkfxfNL9U'>
         This is a guitar cover of the song &ldquo;See You in My Nightmares&rdquo; by Autralian band Daysend. I'm using my first guitar, an ESP LTD MH-50.
-      </Video>
+      </VideoItem> */}
     </>
   )
 }
---
+++ src/pages/videos/VideoData.tsx
@@ -0,0 +1,17 @@
+type VideoDataType = {
+  id: string,
+  description: string,
+}
+
+const video_data: VideoDataType[] = [
+  {
+    id: 'OQUkfxfNL9U',
+    description: `This is a guitar cover of the song &ldquo;See You in My Nightmares&rdquo; by Autralian band Daysend. I'm using my first guitar, an ESP LTD MH-50.`
+  },
+  {
+    id: '6VVKJUojbtE',
+    description: `I covered &ldquo;I Remember&rdquo; by Deadmau5 on guitar, and made a procedural visualiser for the video. Once I've cleaned up de code, I will link to the github repo so you can try it for yourself.`
+  }
+]
+
+export default video_data;
