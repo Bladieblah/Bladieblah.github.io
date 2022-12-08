1053505ef78c640bf5a2a3ad2e58655dfc26debb
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-08 23:06:59 +0100

    Add content to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,22 @@ import styles from './styles/index.module.css'
 
 export default function Home() {
   return (
-    <>
-      {/* <div className={styles.imageWrapper}>
-        <img className={styles.image} src='/me.jpg' alt='idk'/>
-      </div> */}
-
-      <div style={{
-        border: '2px solid white',
-        borderRadius: '10px',
-        margin: '70px',
-        maxWidth: '500px',
-      }}>
-        <p style={{
-          fontSize: 28,
-          margin: '20px',
-        }}>About Me</p>
-        <p style= {{
-          margin: '20px',
-          fontSize: '20px',
-        }}>
-          Welcome to my website! As you can probably tell, I only just started building it so there is still much work left. Until I add more content, please take a look at my CV!
+    <div className={styles.content}>
+      <div className={styles.about}>
+        <h1>About Me</h1>
+        <p>
+          Welcome to my website! I am a Physicist / Data Scientist with a passion for music and programming. I am currently working at a startup called Dyme, where I developed a number of crucial algorithms. 
+        </p>
+        <p>
+          On this website you can find my Curriculum Vitae, a list of my scientific publications and some of my personal projects. These include guitar covers I recorded for youtube and computer generated art.
+        </p>
+        <p>
+          For inquiries, feel free to reach out on linked in or via email, both of which can be found in the footer!
         </p>
       </div>
-    </>
+      <div className={styles.image}>
+        <img src='/me.jpg'/>
+      </div>
+    </div>
   )
 }
