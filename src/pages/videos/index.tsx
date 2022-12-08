6a7ed90071cc1180ef110e8c35e63dd9ab267d36
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-08 20:20:51 +0100

    Rename music page to videos

--- src/pages/videos/index.tsx
+++ src/pages/videos/index.tsx
@@ -1,9 +1,12 @@
-import Head from "next/head";
+import VideoItem from "../../components/VideoItem";
+
+import video_data from "../videos/VideoData";
 
 export default function Videos() {
   return (
-    <div>
-      This vid is mindblowing!
-    </div>
+    <>
+      <h1>Music</h1>
+      {video_data.map((d) => (<VideoItem id={d.id}>{d.description}</VideoItem>))}
+    </>
   )
 }
