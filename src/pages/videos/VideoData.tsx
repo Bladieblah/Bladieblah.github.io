5ea236c19fa68169d1b4e0399924307a72083c7e
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-08 20:26:32 +0100

    Add some vids

--- src/pages/videos/VideoData.tsx
+++ src/pages/videos/VideoData.tsx
@@ -11,7 +11,19 @@ const video_data: VideoDataType[] = [
   {
     id: '6VVKJUojbtE',
     description: `I covered “I Remember” by Deadmau5 on guitar, and made a procedural visualiser for the video. Once I've cleaned up de code, I will link to the github repo so you can try it for yourself.`
-  }
+  },
+  {
+    id: 'eAy6nO2LErY',
+    description: `My entry into the MRNB contest 2021 hosted by Mayones guitars.`
+  },
+  {
+    id: '_b_SOnE6_Gw',
+    description: `A cover of “Gotta Catch 'em All” by Jason Paige, better known as the “Pokemon Theme Song”. It was a lot of fun to make, and is my most watched video so far!`
+  },
+  {
+    id: 'i88voMzY',
+    description: `Complete guitar cover of "A Voice Within" by Intervals. I added the vocals by subtracting the instrumental version of the song from the version with vocals. With a bit of finessing during mixing, it turned out really nice! Using my Ormsby Goliath into the Neural DSP Archetype: Plini and Fortin Nameless Suite plugins. Drums are the Invasion by GGD.`
+  },
 ]
 
 export const get_video_data = (id: string): VideoDataType | null =>  {
