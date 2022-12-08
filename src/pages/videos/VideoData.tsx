bd1e31b720429bd39fdecc8ad2dd57e673621b6d
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-08 20:21:28 +0100

    Add getter function to videoData

--- src/pages/videos/VideoData.tsx
+++ src/pages/videos/VideoData.tsx
@@ -6,12 +6,22 @@ type VideoDataType = {
 const video_data: VideoDataType[] = [
   {
     id: 'OQUkfxfNL9U',
-    description: `This is a guitar cover of the song &ldquo;See You in My Nightmares&rdquo; by Autralian band Daysend. I'm using my first guitar, an ESP LTD MH-50.`
+    description: `This is a guitar cover of the song “See You in My Nightmares” by Autralian band Daysend. I'm using my first guitar, an ESP LTD MH-50.`
   },
   {
     id: '6VVKJUojbtE',
-    description: `I covered &ldquo;I Remember&rdquo; by Deadmau5 on guitar, and made a procedural visualiser for the video. Once I've cleaned up de code, I will link to the github repo so you can try it for yourself.`
+    description: `I covered “I Remember” by Deadmau5 on guitar, and made a procedural visualiser for the video. Once I've cleaned up de code, I will link to the github repo so you can try it for yourself.`
   }
 ]
+
+export const get_video_data = (id: string): VideoDataType | null =>  {
+  for (const video of video_data) {
+    if (video.id === id) {
+      return video;
+    }
+  }
+
+  return null;
+};
 
 export default video_data;
