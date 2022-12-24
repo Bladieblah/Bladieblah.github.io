f8acaa7d8943bfef597cab5845d35a28e7d45cd4
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-24 20:38:56 +0100

    Add buddha pics

--- src/pages/computer-art/buddhabrot.tsx
+++ src/pages/computer-art/buddhabrot.tsx
@@ -2,6 +2,18 @@ import styles from './buddhabrot.module.css'
 
 export default function Buddhabrot() {
   return (
-    <div></div>
+    <div className={styles.content}>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus dolor eu elit faucibus tristique. Etiam tristique urna at risus condimentum, ac malesuada felis sollicitudin. Donec vehicula, velit in mollis placerat, nisl purus imperdiet lectus, et imperdiet ante magna id lorem. Etiam commodo turpis nisl, vitae semper mauris tempor non. Praesent sed felis euismod, aliquet nisi dignissim, semper nisl. Nam ut posuere lacus, nec suscipit tortor. Mauris tortor purus, rhoncus nec mollis vitae, varius non orci. Mauris laoreet pellentesque sodales. Etiam vulputate turpis bibendum, consectetur arcu vel, convallis diam.
+      </p>
+      <img src='/buddhabrot/buddha_4.jpg' alt='Buddha 4'/>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus dolor eu elit faucibus tristique. Etiam tristique urna at risus condimentum, ac malesuada felis sollicitudin. Donec vehicula, velit in mollis placerat, nisl purus imperdiet lectus, et imperdiet ante magna id lorem. Etiam commodo turpis nisl, vitae semper mauris tempor non. Praesent sed felis euismod, aliquet nisi dignissim, semper nisl. Nam ut posuere lacus, nec suscipit tortor. Mauris tortor purus, rhoncus nec mollis vitae, varius non orci. Mauris laoreet pellentesque sodales. Etiam vulputate turpis bibendum, consectetur arcu vel, convallis diam.
+      </p>
+      <img src='/buddhabrot/buddha_0.jpg' alt='Buddha 0'/>
+      <img src='/buddhabrot/buddha_1.jpg' alt='Buddha 1'/>
+      <img src='/buddhabrot/buddha_2.jpg' alt='Buddha 2'/>
+      <img src='/buddhabrot/buddha_3.jpg' alt='Buddha 3'/>
+    </div>
   )
 }
